93eb7b57de9b759e46b4230f72199c160b928caa
alvandi111 <61473798+alvandi111@users.noreply.github.com>
2026-09-23 20:55:44 +0330

    v39: activate verified edits and Persian date display

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='peymanyar-v38';
+const CACHE='peymanyar-v39';
 const ASSETS=['./','index.html','styles.css','workspace-engine.js','collaboration-engine.js','command-engine.js','contract-engine.js','app.js','favicon.svg','manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
